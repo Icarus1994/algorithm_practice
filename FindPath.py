4aa4cc5dc054491af1b537a8af1667c4fbd9a2af
Icarus1994 <littlesec@126.com>
2019-01-18 15:11:47 +0800

    二叉树中和为某一路径的值

--- FindPath.py
+++ FindPath.py
@@ -17,9 +17,32 @@ class Solution:
             return self.liAll
         self.li.append(root.val)
         expectNumber -= root.val
-        if expectNumber==0 and not root.left and not root.right:
-            self.liAll.append(self.li[:])
+        if expectNumber == 0 and not root.left and not root.right:
+            fg = True
+            for i in range(0,len(self.liAll)-1):
+                if len(self.li) > len(self.liAll[i]):
+                    self.liAll.insert(i,self.li[:])
+                    fg = False
+            if fg or not self.liAll:
+                self.liAll.append(self.li[:])
+            print("run",fg,self.li,self.liAll)
+
         self.FindPath(root.left,expectNumber)
         self.FindPath(root.right,expectNumber)
         self.li.pop()
         return self.liAll
+
+root = TreeNode(10)
+node1 = TreeNode(5)
+node2 = TreeNode(12)
+node3 = TreeNode(4)
+node4 = TreeNode(7)
+root.left = node1
+root.right = node2
+node1.left = node3
+node1.right = node4
+print(Solution().FindPath(root,22))
+# 思路
+# 1、首先需要有liAll和li两个全局变量，全局变量不需要作为递归函数findpath的参数，因此作为中间结果的li和liAll不会乱
+# 2、在遍历不同路径时，很多路径之间都有很多公共的节点，如何利用公共节点，一是通过li这个栈来实现公共节点的值的重复利用
+# 二是通过非尾递归，递归到左子树和右子树来实现节点位置的重复利用
