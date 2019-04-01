af4b3d83e23f9fb7ec1db0aef6b8bffbf976c10e
Icarus1994 <littlesec@126.com>
2019-04-01 22:03:50 +0800

    字符串的排列

--- 3Permutation.py
+++ 3Permutation.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import itertools
 class Solution:
-    def Permutation(self, ss):
+    def Permutation1(self, ss):
         # write code here
         result=[]
         if not ss:
@@ -13,6 +13,20 @@ class Solution:
                 if "".join(i) not in result:
                     result.append("".join(i))
         return result
+
+    def Permutation(self, ss):
+        if not ss:
+            return []
+        res = []
+        self.helper(ss, res, '')
+        return sorted(list(set(res)))
+
+    def helper(self, ss, res, path):
+        if not ss:
+            res.append(path)
+        else:
+            for i in range(len(ss)):
+                self.helper(ss[:i] + ss[i+1:], res, path + ss[i])
 
 
 if __name__ == "__main__":
